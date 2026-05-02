2629269779f6dc7479deb069ae4d64a9760b5ee5
Buti Donovan Mkhwanazi <mkwanazibuti082@gmail.com>
2026-05-03 01:21:29 +0200

    feat(docs): add Swagger UI for API documentation

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  // class validator
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true,
@@ -16,7 +18,23 @@ async function bootstrap() {
     }),
   );
 
+  // cookies
   app.use(cookieParser());
+
+  // Swagger UI
+  const config = new DocumentBuilder()
+    .setTitle('My API')
+    .setDescription('API documentation')
+    .setVersion('1.0')
+    .addCookieAuth('access_token')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      withCredentials: true,
+    },
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
